9d555dc258965af21558a8237d2fb965d19884cf
Igor Poplavsky <igor.poplavsky@duallab.com>
2019-08-13 12:58:32 +0300

    Added react-pdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Document, Page } from 'react-pdf';
 import './App.css';
+import test from './test.pdf';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numPages: null,
+            pageNumber: 1,
+        };
+    }
+
+    onDocumentLoadSuccess({ numPages }) {
+        this.setState({ numPages });
+    }
+
+    render() {
+        const { pageNumber, numPages } = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header" />
+                <article>
+                    <Document file={test}
+                              onLoadSuccess={this.onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={pageNumber} />
+                    </Document>
+                </article>
+                <footer>
+                    <p>Page {pageNumber} of {numPages}</p>
+                </footer>
+            </div>
+        );
+    }
 }
 
-export default App;
+export {
+    App
+};
