d232ead4ea76f7cd12d5a874868948bbadc54ad3
Kate Sosinovich <orient.ks@gmail.com>
2019-09-12 09:32:53 +0300

    Updates

---
+++ precache-manifest.51ba63de076bc6cbd52224c6d6d51a48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc050dad4527d6cead65d241bc1ee3d3",
+    "url": "/parse-pdf-demo/index.html"
+  },
+  {
+    "revision": "5d0c51ba790b576856fad80d6b4d64bf",
+    "url": "/parse-pdf-demo/pdf.worker.js"
+  },
+  {
+    "revision": "89166ef29b74b378765a",
+    "url": "/parse-pdf-demo/static/css/2.c83c8d54.chunk.css"
+  },
+  {
+    "revision": "b31fd77036704090afa6",
+    "url": "/parse-pdf-demo/static/css/main.13bd43cf.chunk.css"
+  },
+  {
+    "revision": "89166ef29b74b378765a",
+    "url": "/parse-pdf-demo/static/js/2.8c5e2196.chunk.js"
+  },
+  {
+    "revision": "b31fd77036704090afa6",
+    "url": "/parse-pdf-demo/static/js/main.430608ce.chunk.js"
+  },
+  {
+    "revision": "f1b2b4acf3a3787bfe4e",
+    "url": "/parse-pdf-demo/static/js/runtime~main.8a3ff88d.js"
+  },
+  {
+    "revision": "ba02e324cf16a41b4daa8420cf1b2b0c",
+    "url": "/parse-pdf-demo/static/media/test.ba02e324.pdf"
+  }
+]);
